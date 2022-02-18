65c622c57ddc14baeb1d60e8a7bbf4467634e4a2
METALROK <99863211+METALROK@users.noreply.github.com>
2022-02-19 01:25:16 +0300

    Add files via upload

---
+++ Task2/Task2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+#include <fstream> 
+#include <cmath> 
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    float X, Y, numX, numY, r;
+
+    ifstream File1(argv[1]);
+    ifstream File2(argv[2]);
+
+    File1 >> X >> Y >> r;
+    while (File2) {
+        File2 >> numX >> numY;
+        numX -= X; numY -= Y;
+        if (pow(numX, 2) + pow(numY, 2) == pow(r, 2)) cout << 0; //лежит на окружности
+        if (pow(numX, 2) + pow(numY, 2) < pow(r, 2)) cout << 1; //лежит внутри окружности 
+        if (pow(numX, 2) + pow(numY, 2) > pow(r, 2)) cout << 2; //лежит снаружи окружности 
+        cout << endl;
+        if (File2.eof()) break;
+    }
+
+    return 0;
+}
