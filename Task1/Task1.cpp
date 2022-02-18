f2010055ef6458e147cfc7b9a85b691fc8258d22
METALROK <99863211+METALROK@users.noreply.github.com>
2022-02-19 01:23:37 +0300

    Add files via upload

---
+++ Task1/Task1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std; 
+
+int main(int argc, char *argv[]) {
+    int n, m, lNum;
+
+    ifstream File(argv[1]); 
+    File >> n >> m; 
+    lNum = m; 
+
+
+    if (m == n + 1) cout << 1;
+    else {
+
+        if (m > n) cout << 1;
+        else cout << 1 << lNum;
+
+        while (lNum != 1) {
+            if (lNum > n) lNum = lNum - n;
+            else if (lNum == n) {
+
+                if (m > n) lNum = m - n - 1;
+                else lNum = m - 1;
+
+            } else if (lNum < n) {
+
+                lNum += m - 1;
+                if (lNum > n) lNum = lNum - n;
+
+            }
+
+            if (lNum != 1) cout << lNum;
+        }
+
+    }
+
+    return 0;
+} 
